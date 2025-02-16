a6703f7c57f22b7284476205832c8db41560f272
Dejvidecek <91535222+Dejvidecek@users.noreply.github.com>
2025-02-16 21:48:24 +0100

    oprava en na cs

--- mySession.php
+++ mySession.php
@@ -10,7 +10,7 @@
     else {    
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
